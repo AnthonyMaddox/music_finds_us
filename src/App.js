a1298fd93ee2a0f14b15c4ee2f1462aec8a77234
AnthonyMaddox <anthonyjmaddox@gmail.com>
2022-05-20 20:44:52 -0400

    initialized website with home route and redirect-navigate

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <main>
+          <Routes>
+            <Route path="home" element={<Home />}></Route>
+            <Route path="/" element={<Navigate to="/home" />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+      return (
+        <div>
+          <h6>The Music That Finds Us</h6>
+          <p>Not matter how far we delve into the vast world of sound or how arduously we strain our ears, the songs that hammer our brains and pull at our heart strings seem to come out of absolute nowhere. For it is not us that finds the music, it is the music that finds us.... </p>
+        </div>
+      );
+    }
+  }
+  
+  export default Home;
